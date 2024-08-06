1c62b76618b2d0e341a975dfc812e5baa8fea031
FrankYao747 <frankyao747@gmail.com>
2024-08-06 23:26:54 +1000

    by_FrankYao_06Aug2024_1

--- 1_Web_Learning/Section_16_JavaScript_DescisionMaking/app.js
+++ 1_Web_Learning/Section_16_JavaScript_DescisionMaking/app.js
@@ -66,35 +66,109 @@
 ////////////////////////////////////////////
 
 
-const password = prompt("Please enter a new password");
+// const password = prompt("Please enter a new password");
 
 // Password must be 6+ characters
 
-if (password.length >= 6) {
+// if (password.length >= 6) {
 
 
-    // Password cannnot include space
+//     // Password cannnot include space
 
-    if (password.indexOf(' ') === -1) {
-        console.log("Valid Password!")
-    } else {
-        console.log("Password cannot contain spaces!")
-    }
+//     if (password.indexOf(' ') === -1) {
+//         console.log("Valid Password!")
+//     } else {
+//         console.log("Password cannot contain spaces!")
+//     }
 
-} else {
-    console.log("Password Too short! Must be 6+ characters")
-}
-
-
-
+// } else {
+//     console.log("Password Too short! Must be 6+ characters")
+// }
 
 
 
+////////////////////////////////////////////
+
+
+// const password = prompt("Please enter a new password");
+
+
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid Password!")
+
+// } else {
+//     console.log("Incorrect format for password!")
+// }
 
 
 
+/////////////////////////////////////
+
+// 0 - 5 - FREE
+// 5 - 10 CHILD $10
+// 10 -65 ADULT $20
+// 65+ Senior $10
+
+// const age = prompt("Please enter your age:");
+
+// if (age < 5 && age >= 0) {
+//     console.log("You are a baby, you get in for free!")
+// } else if ((age >= 5 && age < 10) || (age >= 65 && age < 120)) {
+//     console.log("You are a child or a senior, you pay $10. ")
+// } else if (age >= 10 && age < 65) {
+//     console.log("You are an adult, you pay $20.")
+// } else {
+//     console.log("Age Error!")
+// }
 
 
+///////////////////////////////////////////////
+
+// const firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt("Try Again!!!")
+// }
+
+
+
+///////////////////////////////////////////////
+
+
+// const age = prompt("Please enter your age:");
+
+// if (!((age >= 0 && age < 5) || age >= 65)) {
+//     console.log("You are not a baby or a senior!")
+// }
+
+
+////////////////////////////////////////////////
+
+
+// const day = Number(prompt("Enter a day"));
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday!");
+//         break;
+//     case 2:
+//         console.log("Tuesday!");
+//         break;
+//     case 3:
+//         console.log("Wednesday!");
+//         break;
+//     case 4:
+//         console.log("Thursday!");
+//         break;
+//     case 5:
+//         console.log("Friday!");
+//         break;
+//     case 6:
+//     case 7:
+//         console.log("Weekend!");
+//         break;
+//     default:
+//         console.log("Invalid Number!")
+// }
 
 
 
