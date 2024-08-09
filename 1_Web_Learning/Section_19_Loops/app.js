d3af9c1c235d2a4ad2586cb1a3d3d4663b40f195
FrankYao747 <frankyao747@gmail.com>
2024-08-09 23:17:42 +1000

    by_FrankYao_09Aug2024_1

--- 1_Web_Learning/Section_19_Loops/app.js
+++ 1_Web_Learning/Section_19_Loops/app.js
@@ -1,5 +1,73 @@
 
-for (let i = 1; i <= 10; i++) {
-    console.log("I am the loop body!!!");
-    console.log(i);
+// for (let i = 1; i <= 10; i++) {
+//     console.log("I am the loop body!!!");
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 1000; i *= 10) {
+//     console.log(i)
+// }
+
+// for (let i = 25; i >= 0; i -= 5) {
+//     console.log(i)
+// }
+
+// const animals = ['lions', 'tigers', 'bears']
+
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(i, animals[i])
+// }
+
+
+// for (let i = animals.length - 1; i >= 0; i--) {
+//     console.log(i, animals[i])
+// }
+
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (n = 0; n < people.length; n++) {
+//     console.log(`${n + 1} | ${people[n].toUpperCase()}`)
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`i is: ${i}`)
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`    j is: ${j}`)
+//     }
+// }
+
+
+
+// let num = 0;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// }
+
+const SECRET = "BabyHippo";
+
+let guess = null; //prompt("enter the secret code...");
+while (guess !== SECRET) {
+    guess = prompt("enter the secret code...");
 }
+console.log("Congrats! ");
+
+
+
+
+
+
+
