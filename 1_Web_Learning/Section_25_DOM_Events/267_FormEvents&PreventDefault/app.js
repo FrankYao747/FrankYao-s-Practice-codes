8206a6d43b34f740142074dc4c4af4ac66fc99d5
FrankYao747 <frankyao747@gmail.com>
2024-09-07 23:00:54 +1000

    by_FrankYao_07Sep2024_1

--- 1_Web_Learning/Section_25_DOM_Events/267_FormEvents&PreventDefault/app.js
+++ 1_Web_Learning/Section_25_DOM_Events/267_FormEvents&PreventDefault/app.js
@@ -1,24 +1,68 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
 
 form.addEventListener("submit", function (e) {
-    console.log(e);
-    console.log("Submitted!!!");
 
     e.preventDefault();
-    console.log("Submitted 2222!!!");
+    // console.log(input.value);
+    let catName = input.value;
+    let newLI = document.createElement("LI");
+    newLI.innerText = catName;
+    list.append(newLI);
+    input.value = "";
 });
 
 
+/////////////////////////////////////////////////////
+
+
+const tweetForm = document.querySelector("#tweetForm");
+
+const tweetsContainer = document.querySelector("#tweets");
 
 
 
+tweetForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const userNameInput = this.elements.username;
+    const tweetInput = this.elements.tweet;
+    if (userNameInput.value || tweetInput.value) {
+        addTweet(userNameInput.value, tweetInput.value);
+        userNameInput.value = "";
+        tweetInput.value = "";
+    } else {
+        console.log("Null!!!");
+    };
+});
+
+const addTweet = (userName, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(userName);
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`);
+    console.log(newTweet);
+    tweetsContainer.append(newTweet);
+}
 
 
+const qtyforms = document.querySelector('#qtyform');
+const qtylist = document.querySelector('#qtylist');
 
+qtyforms.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const productInput = this.elements.product.value;
+    const qtyInput = this.elements.qty.value;
+    console.log(`${qtyInput} ${productInput}`);
+    const item = document.createElement('li');
+    // const bTag = document.createElement('b');
+    item.innerText = `${qtyInput} ${productInput}`;
+    qtylist.append(item);
+    this.elements.product.value = "";
+    this.elements.qty.value = "";
 
-
-
-
-
+});
 
 
