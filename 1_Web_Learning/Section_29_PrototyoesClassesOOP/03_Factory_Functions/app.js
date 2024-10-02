345ef1f326c6b32c78b34caad9ef75577ada173c
FrankYao747 <frankyao747@gmail.com>
2024-10-02 22:57:11 +1000

    by_FrankYao_02Oct2024_1

--- 1_Web_Learning/Section_29_PrototyoesClassesOOP/03_Factory_Functions/app.js
+++ 1_Web_Learning/Section_29_PrototyoesClassesOOP/03_Factory_Functions/app.js
@@ -79,40 +79,40 @@
 // 3. Passes the newly created object from Step 1 as the this context;
 // 4. Returns this if the function doesn't return its own object.
 
-function Color(r, g, b) {
-	this.r = r;
-	this.g = g;
-	this.b = b;
-	// console.log(this);
-};
+// function Color(r, g, b) {
+// 	this.r = r;
+// 	this.g = g;
+// 	this.b = b;
+// 	// console.log(this);
+// };
 
-Color.prototype.rgb = function () {
-	const { r, g, b } = this;
-	return `rgb(${r},${g},${b})`;
-};
+// Color.prototype.rgb = function () {
+// 	const { r, g, b } = this;
+// 	return `rgb(${r},${g},${b})`;
+// };
 
-Color.prototype.hex = function () {
-	const { r, g, b } = this;
-	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// Color.prototype.hex = function () {
+// 	const { r, g, b } = this;
+// 	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 
-};
-Color.prototype.rgba = function (a = 1.0) {
-	const { r, g, b } = this;
-	return `rgb(${r},${g},${b},${a})`;
-};
+// };
+// Color.prototype.rgba = function (a = 1.0) {
+// 	const { r, g, b } = this;
+// 	return `rgb(${r},${g},${b},${a})`;
+// };
 
-const color1 = new Color(40, 50, 60);
-const color2 = new Color(0, 0, 0);
+// const color1 = new Color(40, 50, 60);
+// const color2 = new Color(0, 0, 0);
 
-color1.hex() // '#28323c'
-color2.hex() // '#000000'
+// color1.hex() // '#28323c'
+// color2.hex() // '#000000'
 
-color1.hex() === color2.hex() // false
+// color1.hex() === color2.hex() // false
 
-color1.hex === color2.hex //true
+// color1.hex === color2.hex //true
 
 
-
+/////////////////////////////////////////////////////////////////////////
 
 // function test(a, b) {
 // 	this.c = a + b;
@@ -150,13 +150,40 @@ color1.hex === color2.hex //true
 // console.log(create(Fun, 18, 'Peter'));
 
 
+///////////////////////////////////////////////////////////////////////////////////////
+
+class Color {
+	constructor(r, g, b, name) {
+		// console.log('Inside Constructor!');
+		// console.log(r, g, b);
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.name = name;
+	}
+	innerRGB() {
+		const { r, g, b } = this;
+		return `${r},${g},${b}`;
+	}
+	rgb() {
+		console.log(`RGB From ${this.name}!`);
+		return `rgb(${this.innerRGB()})`;
+	}
+	rgba(a = 1.0) {
+
+		console.log(`RGBA From ${this.name}!`);
+		return `rgba(${this.innerRGB()},${a})`;
+	}
+	hex() {
+		const { r, g, b, name } = this;
+		console.log(`HEX From ${name}!`);
+		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	}
+}
+
+const red = new Color(255, 67, 89, 'tomato');
 
 
-
-
-
-
-
-
-
+// document.querySelectorAll('h1')[0].style.backgroundColor = c1.greet();
+const white = new Color(255, 255, 255, 'white');
 
