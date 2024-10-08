8c029dc4b045c5d4c2085fff19e335351b5fb4ce
FrankYao747 <frankyao747@gmail.com>
2024-10-08 21:46:22 +1100

    by_FrankYao_08Oct2024_1

---
+++ 1_Web_Learning/Section_31_Node_Intro/greeter.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+for (let arg of args) {
+    console.log(`Hi there, ${arg}`);
+}
+
+
+
+
+// const fs = require('fs');
+// const folderName = process.argv[2] || 'Project';
+ 
+// try {
+//     fs.mkdirSync(folderName);
+//     fs.writeFileSync(`${folderName}/index.html`, '');
+//     fs.writeFileSync(`${folderName}/app.js`, '');
+//     fs.writeFileSync(`${folderName}/styles.css`, '');
+// } catch (e) {
+//     console.log("SOMETHING WENT WRONG!!!");
+//     console.log(e);
+// }
+
+
+
