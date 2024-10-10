eab7b8ddeccccb4a376dccb0aae5fcc8274d3c32
FrankYao747 <frankyao747@gmail.com>
2024-10-10 23:31:49 +1100

    by_FrankYao_10Oct2024_1

---
+++ 1_Web_Learning/Section_32_Node_Modules_NPM/LanGuessrImport/index.js
@@ -0,0 +1,25 @@
+import { franc } from 'franc';
+import langs from 'langs';
+import colors from 'colors';
+
+const input = process.argv[2];
+
+// const langCode = (franc('Alle menslike wesens word vry')) //=> 'afr'
+// const langCode = franc('Alle menneske er fødde til fridom') //=> 'nno'
+
+const langCode = franc(input)
+// const langCode = franc("你好啊 在干嘛")
+// const langCode = franc("한국어, 조선어");
+
+if (langCode === 'und') {
+    console.log(colors.red("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!"));
+} else {
+    const language = langs.where("3", langCode)
+    if (language) {
+        console.log(colors.green(`Our best guess is: ${language.name}`));
+    } else {
+        console.log(colors.red(`SORRY, COULDN'T FIND A LANGUAGE FOR CODE: ${langCode}`));
+    }
+}
+
+
