c0cd72f820afafe041970b198e58deae307f45f9
FrankYao747 <frankyao747@gmail.com>
2024-10-28 23:09:42 +1100

    by_FrankYao_28Oct2024_1

--- 1_Web_Learning/Section_35_REST/RESTDemo/index.js
+++ 1_Web_Learning/Section_35_REST/RESTDemo/index.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    // console.log(req.body);
+    const { username, comment } = req.body;
+    comments.push({ username, comment })
+    res.send("IT Worked!!!");
+})
+
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
