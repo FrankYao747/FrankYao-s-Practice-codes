a8ae1433cb4502acd03af5e38a6a087920051012
FrankYao747 <frankyao747@gmail.com>
2024-10-30 21:37:36 +1100

    by_FrankYao_30Oct2024_1

--- 1_Web_Learning/Section_35_REST/RESTDemo/index.js
+++ 1_Web_Learning/Section_35_REST/RESTDemo/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,18 +10,22 @@ app.set('view engine', 'ejs');
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -37,9 +42,24 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
     // console.log(req.body);
     const { username, comment } = req.body;
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     // res.send("IT Worked!!!");
     res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    // console.log(req.body.comment);
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect(303, '/comments');
 })
 
 
