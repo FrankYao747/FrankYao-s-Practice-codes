a657589485dde921171de5c42c3dffb3b800ae58
FrankYao747 <frankyao747@gmail.com>
2024-11-02 01:40:15 +1100

    by_FrankYao_02Nov2024_1

--- 1_Web_Learning/Section_35_REST/RESTDemo/index.js
+++ 1_Web_Learning/Section_35_REST/RESTDemo/index.js
@@ -10,43 +10,59 @@ app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-let comments = [
-    {
-        id: uuid(),
-        username: 'Todd',
-        comment: 'lol that is so funny!'
-    },
-    {
-        id: uuid(),
-        username: 'Skyler',
-        comment: 'I like to go birdwatching with my dog'
-    },
-    {
-        id: uuid(),
-        username: 'Sk8erBoi',
-        comment: 'Plz delete your account, Todd'
-    },
-    {
-        id: uuid(),
-        username: 'onlysayswoof',
-        comment: 'woof woof woof'
-    }
-]
-//////////////////////
-let clone = []
 
-// 将 user 中所有的属性拷贝到其中
-for (let key in comments) {
-    clone[key] = comments[key];
+let init = () => {
+    let it = [
+        {
+            id: uuid(),
+            username: 'Todd',
+            comment: 'lol that is so funny!'
+        },
+        {
+            id: uuid(),
+            username: 'Skyler',
+            comment: 'I like to go birdwatching with my dog'
+        },
+        {
+            id: uuid(),
+            username: 'Sk8erBoi',
+            comment: 'Plz delete your account, Todd'
+        },
+        {
+            id: uuid(),
+            username: 'onlysayswoof',
+            comment: 'woof woof woof'
+        }
+    ]
+    return it;
 }
+
+let comments = init();
+console.log(comments)
+//////////////////////
+// let clone = []
+
+// // 将 user 中所有的属性拷贝到其中
+// for (let key in comments) {
+//     clone[key] = comments[key];
+// }
 ///////////////////////////////
 
 
 app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments })
+    res.render('comments/index', { comments });
 })
 app.get('/comments/reset', (req, res) => {
-    res.render('comments/index', { comments: clone })
+    let comments_reset = init();
+    comments = [];
+    // console.log("comments_2:");
+    // console.log(comments_2);
+    // res.render('comments/index', { comments: comments_reset });
+    // res.redirect('/comments', { comments: comments_reset });
+    for (let n of comments_reset) {
+        comments.push(n);
+    }
+    res.redirect('/comments');
 })
 
 app.get('/comments/new', (req, res) => {
@@ -55,37 +71,37 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     // console.log(req.body);
-    const { username, comment } = req.body;
-    comments.push({ username, comment, id: uuid() })
+    let { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() });
     // res.send("IT Worked!!!");
-    res.redirect('/comments')
+    res.redirect('/comments');
 })
 
 app.get('/comments/:id', (req, res) => {
-    const { id } = req.params;
-    const comment = comments.find(c => c.id === id);
+    let { id } = req.params;
+    let comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
 })
 
 ///////// id edit ///////////////
 app.get('/comments/:id/edit', (req, res) => {
-    const { id } = req.params;
-    const comment = comments.find(c => c.id === id);
+    let { id } = req.params;
+    let comment = comments.find(c => c.id === id);
     res.render('comments/edit', { comment });
 })
 
 
 app.patch('/comments/:id', (req, res) => {
-    const { id } = req.params;
+    let { id } = req.params;
     // console.log(req.body.comment);
-    const newCommentText = req.body.comment;
-    const foundComment = comments.find(c => c.id === id);
+    let newCommentText = req.body.comment;
+    let foundComment = comments.find(c => c.id === id);
     foundComment.comment = newCommentText;
     res.redirect(303, '/comments');
 })
 
 app.delete('/comments/:id', (req, res) => {
-    const { id } = req.params;
+    let { id } = req.params;
     // const comment = comments.find(c => c.id === id);
     comments = comments.filter(c => c.id !== id);
     res.redirect('/comments');
@@ -97,7 +113,7 @@ app.get('/tacos', (req, res) => {
 
 app.post('/tacos', (req, res) => {
     // console.log(req.body)
-    const { meat, qty } = req.body;
+    let { meat, qty } = req.body;
     res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
 
