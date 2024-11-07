7b00078c714d04e28741abc0121561faf094a4f4
FrankYao747 <frankyao747@gmail.com>
2024-11-07 23:45:31 +1100

    by_FrankYao_07Nov2024_1

---
+++ 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => {
+        console.log("Connection Open!!!")
+    })
+    .catch(err => {
+        console.log("OH NO, Error!!!!")
+        console.log(err)
+    })
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/movieApp');
+//     console.log("Connection Open!!!")
+// }
+
+
+
