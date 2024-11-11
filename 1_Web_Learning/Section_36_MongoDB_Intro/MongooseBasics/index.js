907cfe3a4a9155e8ff2ea1834e4fcb70c71d6cf3
FrankYao747 <frankyao747@gmail.com>
2024-11-11 22:59:49 +1100

    by_FrankYao_11Nov2024_1

--- 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/index.js
+++ 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/index.js
@@ -15,5 +15,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
 //     console.log("Connection Open!!!")
 // }
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+
+// amadeus.save()
 
 
