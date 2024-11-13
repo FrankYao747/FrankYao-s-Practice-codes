35dfa86ff3a1a9ed7d2ea2c4325bc2df30321ddf
FrankYao747 <frankyao747@gmail.com>
2024-11-13 23:28:02 +1100

    by_FrankYao_13Nov2024_1

--- 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/index.js
+++ 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/index.js
@@ -24,9 +24,35 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema);
 
-const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+// const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
 
 
 // amadeus.save()
+
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+// ])
+//     .then(data => {
+//         console.log("It Worked!!!");
+//         console.log(data);
+//     })
+
+
+
+// Movie.find({rating: 'PG-13'}).then(data => console.log(data))
+
+// Movie.find({year: {$lt:1990}}).then(data => console.log(data))
+
+// Movie.findOne({year: {$gte:1990}}).then(data => console.log(data))
+
+
+//// by id:
+// Movie.find({_id:'673434421cf33c99ab86749a'}).then(m => console.log(m))
+
+
 
 
