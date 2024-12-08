0f9c5c133870a2e9a060e6627173b04646c44145
FrankYao747 <frankyao747@gmail.com>
2024-12-08 19:11:00 +1100

    by_FrankYao_08Dec2024_1

--- 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/products.js
+++ 1_Web_Learning/Section_36_MongoDB_Intro/MongooseBasics/products.js
@@ -49,19 +49,48 @@ const productSchema = new mongoose.Schema({
     }
 });
 
+// productSchema.methods.greet = function () {
+//     console.log("Hello!!! HI! HOWDY!");
+//     console.log(`- from ${this.name}`)
+// }
+
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+productSchema.methods.addCategory = function (newCat) {
+    this.categories.push(newCat);
+    return this.save;
+}
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'S' });
-bike.save()
-    .then(data => {
-        console.log("It worked!");
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH No, Error!!!");
-        // console.log(err.errors.name.properties.message);
-        console.log(err);
-    })
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: 'Mountain Bike' });
+    console.log(foundProduct)
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+
+    await foundProduct.addCategory('Outdoors');
+    console.log(foundProduct);
+
+}
+
+findProduct();
+
+
+// const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'S' });
+// bike.save()
+//     .then(data => {
+//         console.log("It worked!");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH No, Error!!!");
+//         // console.log(err.errors.name.properties.message);
+//         console.log(err);
+//     })
 
 // Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -19.99 }, { new: true, runValidators: true })
 //     .then(data => {
