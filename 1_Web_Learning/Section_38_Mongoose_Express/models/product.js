58ec47d078441f1d20de24f3980757297c625f08
FrankYao747 <frankyao747@gmail.com>
2024-12-23 21:18:58 +1100

    by_FrankYao_23Dec2024_1

--- 1_Web_Learning/Section_38_Mongoose_Express/models/product.js
+++ 1_Web_Learning/Section_38_Mongoose_Express/models/product.js
@@ -12,11 +12,14 @@ const productSchema = new mongoose.Schema({
     },
     category: {
         type: String,
+        lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
     }
 })
 
 const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
 
 
 
