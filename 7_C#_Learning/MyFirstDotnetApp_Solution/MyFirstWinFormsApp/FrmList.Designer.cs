96013ed8dc53e31c285f932a9475f8222b1cdb6a
FrankYao747 <frankyao747@gmail.com>
2025-02-23 22:06:32 +1100

    by_FrankYao_23Feb2025_1

--- 7_C#_Learning/MyFirstDotnetApp_Solution/MyFirstWinFormsApp/FrmList.Designer.cs
+++ 7_C#_Learning/MyFirstDotnetApp_Solution/MyFirstWinFormsApp/FrmList.Designer.cs
@@ -28,72 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            dgvPerson = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
             panel1 = new Panel();
-            btnBinding = new Button();
             btnCancel = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            btnBinding = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvPerson).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dgvPerson
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1081, 482);
-            dataGridView1.TabIndex = 0;
+            dgvPerson.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvPerson.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPerson.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dgvPerson.Location = new Point(12, 12);
+            dgvPerson.Name = "dgvPerson";
+            dgvPerson.RowHeadersWidth = 51;
+            dgvPerson.Size = new Size(1094, 456);
+            dgvPerson.TabIndex = 0;
+            dgvPerson.RowHeaderMouseClick += dgvPerson_RowHeaderMouseClick;
+            // 
+            // Column1
+            // 
+            Column1.DataPropertyName = "Id";
+            Column1.HeaderText = "ID";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.DataPropertyName = "Name";
+            Column2.HeaderText = "Name";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.DataPropertyName = "Age";
+            Column3.HeaderText = "Age";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.Width = 125;
             // 
             // panel1
             // 
+            panel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel1.BorderStyle = BorderStyle.FixedSingle;
             panel1.Controls.Add(btnCancel);
             panel1.Controls.Add(btnBinding);
-            panel1.Location = new Point(12, 510);
+            panel1.Location = new Point(12, 484);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1081, 177);
+            panel1.Size = new Size(1094, 177);
             panel1.TabIndex = 1;
-            // 
-            // btnBinding
-            // 
-            btnBinding.Location = new Point(837, 65);
-            btnBinding.Name = "btnBinding";
-            btnBinding.Size = new Size(94, 29);
-            btnBinding.TabIndex = 0;
-            btnBinding.Text = "Binding";
-            btnBinding.UseVisualStyleBackColor = true;
             // 
             // btnCancel
             // 
+            btnCancel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             btnCancel.Location = new Point(970, 65);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(94, 29);
+            btnCancel.Size = new Size(90, 29);
             btnCancel.TabIndex = 1;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = true;
             btnCancel.Click += btnCancel_Click;
             // 
+            // btnBinding
+            // 
+            btnBinding.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            btnBinding.Location = new Point(837, 65);
+            btnBinding.Name = "btnBinding";
+            btnBinding.Size = new Size(90, 29);
+            btnBinding.TabIndex = 0;
+            btnBinding.Text = "Binding";
+            btnBinding.UseVisualStyleBackColor = true;
+            btnBinding.Click += btnBinding_Click;
+            // 
             // FrmList
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1105, 699);
+            ClientSize = new Size(1118, 673);
             Controls.Add(panel1);
-            Controls.Add(dataGridView1);
+            Controls.Add(dgvPerson);
             Name = "FrmList";
             Text = "FrmList";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPerson).EndInit();
             panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dgvPerson;
         private Panel panel1;
         private Button btnCancel;
         private Button btnBinding;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
     }
 }
--- 7_C#_Learning/MyFirstDotnetApp_Solution/MyFirstWinFormsApp/FrmList.cs
+++ 7_C#_Learning/MyFirstDotnetApp_Solution/MyFirstWinFormsApp/FrmList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstWinFormsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace MyFirstWinFormsApp
 {
     public partial class FrmList : Form
     {
+        List<Person> persons;
         public FrmList()
         {
             InitializeComponent();
@@ -20,6 +22,51 @@ namespace MyFirstWinFormsApp
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnBinding_Click(object sender, EventArgs e)
+        {
+            persons = new List<Person>();
+            Person p1 = new Person();
+            p1.Name = "Frank";
+            p1.Age = 35;
+            p1.Id = 1;
+            persons.Add(p1);
+
+            Person p2 = new Person()
+            {
+                Name = "Peter",
+                Age = 31,
+                Id = 2
+            };
+            persons.Add(p2);
+
+            persons.Add(new Person()
+            {
+                Name = "Davin",
+                Age = 30,
+                Id = 3
+            });
+            persons.Add(new Person()
+            {
+                Name = "Leo",
+                Age = 33,
+                Id = 4
+            });
+
+            dgvPerson.DataSource = persons;
+        }
+
+        private void dgvPerson_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            if(persons != null)
+            {
+                dgvPerson.DataSource = new List<Person>();
+                persons.RemoveAt(rowIndex);
+                dgvPerson.DataSource = persons;
+            }
+
         }
     }
 }
